b4311afb35aa75a63bc59b842b4242a0ac84e9bd
Felipe Lopes Lage <felipe.lage@easynvest.com.br>
2020-08-03 14:04:56 -0300

    fix:ajust port

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var http = require('http')
-
+const http = require('http')
+const port = process.env.PORT || 8080
 try {
   http
     .createServer(function (req, res) {
       res.writeHead(200, { 'Content-Type': 'text/plain' })
       res.end('Hello World!')
     })
-    .listen(8080)
-  console.log('Listening to port 8080...')
+    .listen(port)
+  console.log(`Listening to port ${port}...`)
 } catch (e) {
   console.error(e)
 }
