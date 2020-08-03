8244ba67a5b14d4591734a4fdaa552308e95a312
Felipe Lopes Lage <felipe.lage@easynvest.com.br>
2020-08-03 15:13:21 -0300

    feat:add algorithmia, bodyParser and nodemon plugins, first test with algorithmia

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const dotenv = require('dotenv')
-const http = require('http')
 const app = require('express')()
+const bodyParser = require('body-parser')
+const Algorithmia = require('algorithmia')
+
 const port = process.env.PORT || 8080
+
 dotenv.config()
+
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.end('Hello World from Express!')
 })
 
+app.get('/algo', async (req, res) => {
+  const { script, targetURL, target } = req.body
+  const { result } = await Algorithmia.client(process.env.ALGORITHMIA_CLIENT_KEY)
+    .algo(`${script}?timeout=${process.env.ALGORITHMIA_CLIENT_TIMEOUT}`) // timeout is optional
+    .pipe(targetURL || target)
+  res.json({ result })
+})
+
 app.listen(port, () => {
   console.log(`Listening to port ${port}...`)
-  console.log(process.env.ALGORITHMIA_CLIENT_KEY)
 })
